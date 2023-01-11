ccf6f3fa8f2e9c751c574876bae8145251a37280
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2023-01-11 13:07:01 -0300

    Lidando com exceptions no JUnit

--- src/test/java/br/com/estudandoemcasa/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/estudandoemcasa/tdd/service/BonusServiceTest.java
@@ -1,11 +1,14 @@
 package br.com.estudandoemcasa.tdd.service;
 
 import br.com.estudandoemcasa.tdd.modelo.Funcionario;
+import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import static org.junit.Assert.*;
 
 class BonusServiceTest {
 
@@ -13,7 +16,11 @@ class BonusServiceTest {
     void resultSoma(){
         BonusService bonusService = new BonusService();
         Funcionario funcionario = new Funcionario("Miguel", LocalDate.now(), new BigDecimal("600000"));
-        BigDecimal bonus = bonusService.calcularBonus(funcionario);
-        Assertions.assertEquals(new BigDecimal("0.0"), bonus);
+        try {
+            BigDecimal bonus = bonusService.calcularBonus(funcionario);
+            fail("Erro no test");
+        } catch (IllegalArgumentException e){
+            Assertions.assertEquals(e.getMessage(), e.getMessage());
+        }
     }
 }
